c80de2fa4ed46e09bd5b6ff83a208b00ce0d22c5
jalzamora21 <94610731+jalzamora21@users.noreply.github.com>
2022-01-09 00:53:44 +0100

    Added project files

---
+++ backend_private/src/main/java/com/daw/ticketsdaw/TicketsDawApplication.java
@@ -0,0 +1,13 @@
+package com.daw.ticketsdaw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketsDawApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TicketsDawApplication.class, args);
+    }
+
+}
---
+++ backend_private/src/test/java/com/daw/ticketsdaw/TicketsDawApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daw.ticketsdaw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketsDawApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
