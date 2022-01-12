604b6c51493072cf020a6b3eeb1f47540b165485
jalzamora21 <94610731+jalzamora21@users.noreply.github.com>
2022-01-12 11:50:44 +0100

    Disabled Converter + Added Organizador repository

--- backend_private/src/main/java/com/daw/ticketsdaw/WebConfig.java
+++ backend_private/src/main/java/com/daw/ticketsdaw/WebConfig.java
@@ -9,6 +9,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addFormatters(FormatterRegistry registry){
-        registry.addConverter(new StringToOrganitzadorConverter());
+        //registry.addConverter(new StringToOrganitzadorConverter());
     }
 }
