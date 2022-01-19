8d8e36a30f8441c5feb3a4f63ab39a2b18e20934
Arteux <55326807+Arteux@users.noreply.github.com>
2022-01-19 19:35:44 +0100

    Fixed wrong import for @Transactional annotation

--- backend_private/src/main/java/com/daw/ticketsdaw/Services/SalaServiceImpl.java
+++ backend_private/src/main/java/com/daw/ticketsdaw/Services/SalaServiceImpl.java
@@ -4,8 +4,8 @@ import com.daw.ticketsdaw.Entities.Sala;
 import com.daw.ticketsdaw.Repositories.SalaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import javax.transaction.Transactional;
 import java.util.List;
 import java.util.Optional;
 
