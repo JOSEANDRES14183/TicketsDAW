2b97b665f20df9e4de72994d12292721b9ca3317
JOSEANDRES14183 <jreyes14183@alumnes.iesmanacor.cat>
2022-01-30 21:03:18 +0100

    entity sesion numerada arreglada

--- backend_private/src/main/java/com/daw/ticketsdaw/Entities/SesionNumerada.java
+++ backend_private/src/main/java/com/daw/ticketsdaw/Entities/SesionNumerada.java
@@ -2,18 +2,16 @@ package com.daw.ticketsdaw.Entities;
 
 import lombok.Data;
 
-import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 
 @Data
 @Entity
-@Table(name = "sesion_no_numerada")
+@Table(name = "sesion_numerada")
 public class SesionNumerada extends Sesion{
 
     @NotNull
-    @Column(name = "plazas_max")
-    private int plazasMax;
+    private float precio;
 
 }
