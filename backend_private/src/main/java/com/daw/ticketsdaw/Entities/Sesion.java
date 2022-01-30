0992b7ec685b5e6675d07bb0e7e35901e140bd16
JOSEANDRES14183 <jreyes14183@alumnes.iesmanacor.cat>
2022-01-30 19:23:25 +0100

    entidad padre sesion terminada

---
+++ backend_private/src/main/java/com/daw/ticketsdaw/Entities/Sesion.java
@@ -0,0 +1,55 @@
+package com.daw.ticketsdaw.Entities;
+
+import lombok.Data;
+import lombok.ToString;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.util.Date;
+
+@Data
+@Entity
+@Table(name = "sesion")
+@Inheritance(strategy = InheritanceType.JOINED)
+
+public abstract class Sesion {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @NotNull
+    @Column(name = "entradas_max")
+    private int entradasMax;
+
+    @NotNull
+    @Column(name = "fecha_fin_venta")
+    private Date fechaFinVenta;
+
+    @NotNull
+    @Column(name = "fecha_ini")
+    private Date fechaIni;
+
+    @NotNull
+    @Column(name = "fecha_ini")
+    private Date fechaFin;
+
+    @NotNull
+    @Column(name = "esta_oculto")
+    private boolean estaOculto;
+
+    @ToString.Exclude
+    @ManyToOne
+    @JoinColumn(name = "id_evento", referencedColumnName = "id")
+    @NotNull
+    private Evento evento;
+
+    @ToString.Exclude
+    @ManyToOne
+    @JoinColumn(name = "id_sala", referencedColumnName = "id")
+    @NotNull
+    private Sala sala;
+
+
+
+}
