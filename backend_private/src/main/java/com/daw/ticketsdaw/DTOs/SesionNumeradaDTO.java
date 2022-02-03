99edf25ddcc86b61067f6d76eb97e65027003aa0
jalzamora21 <94610731+jalzamora21@users.noreply.github.com>
2022-02-03 13:56:21 +0100

    Added DateTimeFormat to SesionNumeradaDTO (Fixed can't create Sesion Numerada)

--- backend_private/src/main/java/com/daw/ticketsdaw/DTOs/SesionNumeradaDTO.java
+++ backend_private/src/main/java/com/daw/ticketsdaw/DTOs/SesionNumeradaDTO.java
@@ -1,11 +1,17 @@
 package com.daw.ticketsdaw.DTOs;
 import com.daw.ticketsdaw.Entities.Sala;
 import lombok.Data;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import javax.persistence.Transient;
 import javax.validation.constraints.NotNull;
 import java.util.Date;
 
 @Data
 public class SesionNumeradaDTO {
+
+    @Transient
+    private final String dateTimeFormat = "yyyy-MM-dd'T'HH:mm";
 
     private Integer id;
 
@@ -13,12 +19,15 @@ public class SesionNumeradaDTO {
     private int entradasMax;
 
     @NotNull
+    @DateTimeFormat(pattern = dateTimeFormat)
     private Date fechaFinVenta;
 
     @NotNull
+    @DateTimeFormat(pattern = dateTimeFormat)
     private Date fechaIni;
 
     @NotNull
+    @DateTimeFormat(pattern = dateTimeFormat)
     private Date fechaFin;
 
     @NotNull
