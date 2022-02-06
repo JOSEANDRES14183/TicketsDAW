9455a39ee09d54476f536757a8ec7d5f13470812
Arteux <55326807+Arteux@users.noreply.github.com>
2022-02-06 23:05:37 +0100

    Started implementation

--- backend_private/src/main/java/com/daw/ticketsdaw/Services/SesionService.java
+++ backend_private/src/main/java/com/daw/ticketsdaw/Services/SesionService.java
@@ -2,9 +2,13 @@ package com.daw.ticketsdaw.Services;
 
 import com.daw.ticketsdaw.Entities.Sesion;
 import com.daw.ticketsdaw.Repositories.SesionRepository;
+import org.apache.commons.lang3.time.DateUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import javax.validation.ConstraintViolationException;
+import java.util.Date;
 
 @Service
 @Transactional
@@ -14,10 +18,23 @@ public class SesionService {
     private SesionRepository sesionRepository;
 
     public void save(Sesion sesion){
-        sesionRepository.save(sesion);
+        Date startDate = sesion.getFechaIni();
+        int duracionEstandar = sesion.getEvento().getDuracionEstandar();
+        Date endDate = DateUtils.addMinutes(sesion.getFechaIni(), duracionEstandar);
+        if(sesionRepository.countByFechaIniBetween(startDate, endDate) > 0)
+            System.out.println("Sesion save failed");
+        else
+            sesionRepository.save(sesion);
     }
 
-    public Sesion read(int id){ return sesionRepository.findById(id).get();}
+    private boolean overlap(Date startDate1, int duration1, Date startDate2, int duration2){
+        //Implement this logic https://stackoverflow.com/questions/3269434/whats-the-most-efficient-way-to-test-if-two-ranges-overlap
+        //(StartA <= EndB) and (EndA >= StartB)
+    }
+
+    public Sesion read(int id){
+        return sesionRepository.findById(id).get();
+    }
 
     public void delete(Sesion sesion){
         sesionRepository.delete(sesion);
