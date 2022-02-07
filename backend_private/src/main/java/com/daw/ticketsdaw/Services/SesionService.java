4aa3e4d8914e8112c61ff516d94ba397cb0ebc91
jalzamora21 <94610731+jalzamora21@users.noreply.github.com>
2022-02-07 10:16:46 +0100

    Added overlap check + warning when saving with overlap

--- backend_private/src/main/java/com/daw/ticketsdaw/Services/SesionService.java
+++ backend_private/src/main/java/com/daw/ticketsdaw/Services/SesionService.java
@@ -9,6 +9,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import javax.validation.ConstraintViolationException;
 import java.util.Date;
+import java.util.List;
 
 @Service
 @Transactional
@@ -17,19 +18,37 @@ public class SesionService {
     @Autowired
     private SesionRepository sesionRepository;
 
-    public void save(Sesion sesion){
-        Date startDate = sesion.getFechaIni();
-        int duracionEstandar = sesion.getEvento().getDuracionEstandar();
-        Date endDate = DateUtils.addMinutes(sesion.getFechaIni(), duracionEstandar);
-        if(sesionRepository.countByFechaIniBetween(startDate, endDate) > 0)
-            System.out.println("Sesion save failed");
-        else
-            sesionRepository.save(sesion);
+    /**
+     * Saves a Sesion object to the database and returns whether it has saved without overlapping with another Sesion or not
+     * @param sesion    the Sesion object to save to DB
+     * @return          returns false if the Sesion has been saved, but it overlaps with another Sesion on the DB
+     */
+    public boolean save(Sesion sesion){
+        //Save date availability before saving the Sesion to the DB, if not, it will collide with itself when checking availability
+        boolean isDateAvailable = checkDateAvailability(sesion);
+        sesionRepository.save(sesion);
+        return isDateAvailable;
     }
 
-    private boolean overlap(Date startDate1, int duration1, Date startDate2, int duration2){
-        //Implement this logic https://stackoverflow.com/questions/3269434/whats-the-most-efficient-way-to-test-if-two-ranges-overlap
-        //(StartA <= EndB) and (EndA >= StartB)
+    private boolean checkDateAvailability(Sesion newSesion){
+        //Possible optimization https://stackoverflow.com/questions/22007341/spring-jpa-selecting-specific-columns
+        List<Sesion> sesiones = sesionRepository.findAll();
+        for (var sesion : sesiones) {
+            if(checkSesionOverlap(sesion, newSesion))
+                return false;
+        }
+        return true;
+    }
+
+    private boolean checkSesionOverlap(Sesion sesion1, Sesion sesion2){
+        var startDate1 = sesion1.getFechaIni();
+        var endDate1 = DateUtils.addMinutes(sesion1.getFechaIni(), sesion1.getDuracion());
+        var startDate2 = sesion2.getFechaIni();
+        var endDate2 = DateUtils.addMinutes(sesion2.getFechaIni(), sesion2.getDuracion());
+        if((startDate1.before(endDate2) || startDate1.equals(endDate2)) && (endDate1.after(startDate2) || endDate1.equals(startDate2)))
+            return true;
+        else
+            return false;
     }
 
     public Sesion read(int id){
