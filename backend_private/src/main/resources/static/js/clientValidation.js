de92770d3f2190a59f5eb2ae151256e96952e591
Arteux <55326807+Arteux@users.noreply.github.com>
2022-02-13 22:04:35 +0100

    Moved client validation with password into separate JS file

--- backend_private/src/main/resources/static/js/clientValidation.js
+++ backend_private/src/main/resources/static/js/clientValidation.js
@@ -9,44 +9,12 @@
     Array.prototype.slice.call(forms)
         .forEach(function (form) {
             form.addEventListener('submit', function (event) {
-                let isRepeatedPasswordValid = checkRepeatedPassword()
-
-                if (!form.checkValidity() || !isRepeatedPasswordValid) {
+                if (!form.checkValidity()) {
                     event.preventDefault()
                     event.stopPropagation()
                 }
 
                 form.classList.add('was-validated')
-
-                if(!isRepeatedPasswordValid){
-                    resetFormValidation()
-                }
             }, false)
         })
 })()
-
-document.getElementById("passwordHashRepeat").addEventListener("keyup", checkRepeatedPassword)
-document.getElementById("passwordHash").addEventListener("keyup", checkRepeatedPassword)
-
-function checkRepeatedPassword(){
-    let elementRepeat = document.getElementById("passwordHashRepeat");
-    elementRepeat.classList.remove("is-valid")
-    elementRepeat.classList.remove("is-invalid")
-
-    if(document.getElementById("passwordHash").value === elementRepeat.value){
-        elementRepeat.classList.add("is-valid");
-        return true;
-    }
-    else{
-        elementRepeat.classList.add("is-invalid");
-        resetFormValidation();
-        return false;
-    }
-}
-
-function resetFormValidation(){
-    let forms = document.getElementsByTagName("form")
-    for (let i = 0; i < forms.length; i++) {
-        forms[i].classList.remove("was-validated")
-    }
-}
