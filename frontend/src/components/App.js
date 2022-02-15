77ed1870edb7985a09103538d3f37c37f5c32f7c
JOSEANDRES14183 <jreyes14183@alumnes.iesmanacor.cat>
2022-02-15 12:48:38 +0100

    layout básico creado

---
+++ frontend/src/components/App.js
@@ -0,0 +1,19 @@
+import Header from './Header.js';
+import Footer from './Footer.js';
+
+function App() {
+  return (
+    <>
+        <Header />
+        <main>
+            <section>
+
+            </section>
+        </main>
+        <Footer />
+    </>
+  );
+
+}
+
+export default App;
---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,9 @@
+function Footer(){
+    return(
+        <footer className={"bg-secondary text-white w-100 p-4 mt-auto"}>
+            <p className="text-center">Ⓒ 2021-2022, all rights reserved</p>
+        </footer>
+    );
+}
+
+export default Footer;
