58aea98d1b8230f5ae2593a19a01ec966c249438
JOSEANDRES14183 <jreyes14183@alumnes.iesmanacor.cat>
2022-02-19 18:53:32 +0100

    añadido enlace de como controlar json a EventoController.php

--- backend_public/app/Http/Controllers/EventoController.php
+++ backend_public/app/Http/Controllers/EventoController.php
@@ -5,11 +5,17 @@ namespace App\Http\Controllers;
 use App\Models\Evento;
 use Illuminate\Http\Request;
 
+//Tutorial para controlar que datos aparecen en el JSON
+//https://laravel.com/docs/9.x/eloquent-resources
+
 class EventoController extends Controller
 {
     public function index()
     {
-        return Evento::with('sesiones.sala.butacas')->with('fotoPerfil')->with('categoria')->get();
+        return Evento::with('sesiones.sala.butacas')
+            ->with('fotoPerfil')
+            ->with('categoria')
+            ->get();
     }
 
     public function show($id)
