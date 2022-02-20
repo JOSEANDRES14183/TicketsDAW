cdc81cf18488669c797f2a38c7943dd43ab81e27
JOSEANDRES14183 <jreyes14183@alumnes.iesmanacor.cat>
2022-02-20 11:09:31 +0100

    añadido organizador y funcional. UsuarioResource implementado pero no funcional

--- backend_public/app/Models/Evento.php
+++ backend_public/app/Models/Evento.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Http\Resources\UsuarioResource;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -10,8 +11,9 @@ class Evento extends Model
     use HasFactory;
 
     protected $table = 'evento';
-
     protected $primaryKey = 'id';
+
+    protected $with = ['organizador'];
 
     //It is necessary to define if the primary key is not incremental
     //https://laravel.com/docs/9.x/eloquent#introduction
@@ -30,6 +32,11 @@ class Evento extends Model
     public function categoria()
     {
         return $this->belongsTo(Categoria::class,'id_categoria');
+    }
+
+    public function organizador()
+    {
+        return new UsuarioResource($this->belongsTo(Usuario::class,'id_organizador'));
     }
 
 
---
+++ backend_public/app/Models/Usuario.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Usuario extends Model
+{
+    use HasFactory;
+
+    protected $table = 'usuario';
+    protected $primaryKey = 'id';
+
+    protected $with = ['organizadorData'];
+
+    public function organizadorData()
+    {
+        return $this->hasOne(OrganizadorData::class,'id');
+    }
+
+}
