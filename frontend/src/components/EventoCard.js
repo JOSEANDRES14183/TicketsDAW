de59f512267c42c84dd98b4c93f2d6f5318a3d64
JOSEANDRES14183 <jreyes14183@alumnes.iesmanacor.cat>
2022-02-21 19:57:29 +0100

    fixed protocol prefix

--- frontend/src/components/EventoCard.js
+++ frontend/src/components/EventoCard.js
@@ -7,7 +7,7 @@ function EventoCard(props){
                 <div className={"d-flex border border-secondary rounded-25 h-100 shadow-sm position-relative"}>
                     <Link to={"/"+props.evento.id} className={"w-100 h-100 position-absolute top-0 left-0"}></Link>
                     <img className={"rounded-25 border-1 border-end img-fluid"}
-                         src={process.env.REACT_APP_API_PROTOCOL+process.env.REACT_APP_API_HOST + '/api/media/' +props.evento.foto_perfil.nombre_archivo}/>
+                         src={process.env.REACT_APP_API_PROTOCOL_PREFIX+process.env.REACT_APP_API_HOST + '/api/media/' +props.evento.foto_perfil.nombre_archivo}/>
                     <div className={"d-flex flex-column p-3"}>
                         <h6>{props.evento.titulo}</h6>
                         <p className={"text-secondary fw-bold"}>{props.evento.categoria.nombre}</p>
