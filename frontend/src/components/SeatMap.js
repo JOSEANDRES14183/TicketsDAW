d59d2598f4467665007c426f256c11b321e5a51e
Arteux <55326807+Arteux@users.noreply.github.com>
2022-03-02 19:58:00 +0100

    Seat map selection mostly works

--- frontend/src/components/SeatMap.js
+++ frontend/src/components/SeatMap.js
@@ -6,116 +6,79 @@ const MARGIN = { top: 30, right: 30, bottom: 30, left: 30 };
 
 //https://www.react-graph-gallery.com/scatter-plot
 function SeatMap({seats}) {
-    const ref = useD3(
-        (svg) => {
-            const multiplyFactor = 48;
+    const renderFn = (svg) => {
+        const multiplyFactor = 48;
 
-            svg
-                .select("g.plotArea")
-                .selectAll("circle")
-                .remove()
-                .data(seats)
-                .enter()
-                .append("circle")
-                .attr("r", 16)
-                .attr("opacity", 1)
-                .attr("stroke", "#000")
-                .attr("fillOpacity", 0.7)
-                .attr("strokeWidth", 1)
-                .attr("fill", (d) => d.seleccionada ? "#B8FFF9" : "#9a6fb0")
-                .attr("cx", (d) => d.pos_x*multiplyFactor)
-                .attr("cy", (d) => d.pos_y*multiplyFactor)
-                .attr("onclick", (d) => seatClick(d))
+        //https://github.com/d3/d3-selection/blob/v3.0.0/README.md#selection_on
 
-            function handleZoom(e) {
-                svg.select('g')
-                    .attr('transform', e.transform);
-            }
+        svg
+            .select("g.plotArea")
+            .selectAll("circle")
+            .data(seats)
+            .enter()
+            .append("circle")
+            .attr("r", 16)
+            .attr("opacity", 1)
+            .attr("stroke", "#000")
+            .attr("fillOpacity", 0.7)
+            .attr("strokeWidth", 1)
+            .attr("fill", (d) => d.seleccionada ? "#B8FFF9" : "#9a6fb0")
+            .attr("cx", (d) => d.pos_x*multiplyFactor)
+            .attr("cy", (d) => d.pos_y*multiplyFactor)
+            .on("click", function(event,datum){
+                seatClick(datum)
+            })
+        //(d) => seatClick(d)
 
-            let zoom = d3.zoom()
-                .on('zoom', handleZoom);
+        svg
+            .select("g.plotArea")
+            .selectAll("circle")
+            .data(seats)
+            .exit()
+            .remove()
 
-            svg
-                .call(zoom);
+        svg
+            .select("g.plotArea")
+            .selectAll("circle")
+            .data(seats)
+            .transition()
+            .duration(100) //TODO: Remove this duration
+            .attr("fill", (d) => d.seleccionada ? "#B8FFF9" : "#9a6fb0")
+            .attr("cx", (d) => d.pos_x*multiplyFactor)
+            .attr("cy", (d) => d.pos_y*multiplyFactor)
 
-            const allShapes = seats.map((d, i) => {
-                return (
-                    <circle
-                        key={i}
-                        r={16}
-                        cx={d.pos_x*multiplyFactor}
-                        cy={d.pos_y*multiplyFactor}
-                        opacity={1}
-                        stroke="#000"
-                        fill={d.seleccionada ? "#B8FFF9" : "#9a6fb0"}
-                        fillOpacity={0.7}
-                        strokeWidth={1}
-                        onClick={() => seatClick(i)}
-                    />
-                );
-        }, [seats.length]
-    )
+        function handleZoom(e) {
+            svg.select('g')
+                .attr('transform', e.transform);
+        }
 
-    // const axesRef = useRef(null);
-    // const boundsWidth = width - MARGIN.right - MARGIN.left;
-    // const boundsHeight = height - MARGIN.top - MARGIN.bottom;
+        let zoom = d3.zoom()
+            .on('zoom', handleZoom);
 
-    // // Y axis
-    // const yScale = useMemo(() => {
-    //     const [min, max] = d3.extent(seats.map((d) => d.pos_x));
-    //     return d3.scaleLinear().domain([min, max]).range([boundsHeight, 0]);
-    // }, [seats, height]);
-    //
-    // // X axis
-    // const xScale = useMemo(() => {
-    //     const [min, max] = d3.extent(seats.map((d) => d.pos_y));
-    //     return d3.scaleLinear().domain([min, max]).range([0, boundsWidth]);
-    // }, [seats, width]);
+        svg
+            .call(zoom);
+    }
 
-    // Render the X and Y axis using d3.js, not react
-    // useEffect(() => {
-    //     const svgElement = d3.select(axesRef.current);
-    //     svgElement.selectAll("*").remove();
-    //     const xAxisGenerator = d3.axisBottom(xScale);
-    //     svgElement
-    //         .append("g")
-    //         .attr("transform", "translate(0," + boundsHeight + ")")
-    //         .call(xAxisGenerator);
-    //
-    //     const yAxisGenerator = d3.axisLeft(yScale);
-    //     svgElement.append("g").call(yAxisGenerator);
-    // }, [xScale, yScale, boundsHeight]);
+    const ref = useD3(renderFn, [seats]);
 
     function seatClick(seat){
         seat.seleccionada = true;
+        renderFn(d3.select(ref.current))
     }
-
-    });
 
     return(
         <svg
             ref={ref}
-            // width={width} height={height}
-            // id="d3graph"
         style={{
             height: "100%",
             width: "100%"
         }}>
             <g
-                // width={boundsWidth}
-                // height={boundsHeight}
                 className="plotArea"
                 transform={`translate(${[MARGIN.left, MARGIN.top].join(",")})`}
             >
                 {/*{allShapes}*/}
             </g>
-            {/* Second is for the axes */}
-            {/*<g*/}
-            {/*    width={boundsWidth}*/}
-            {/*    height={boundsHeight}*/}
-            {/*    ref={axesRef}*/}
-            {/*    transform={`translate(${[MARGIN.left, MARGIN.top].join(",")})`}*/}
-            {/*/>*/}
         </svg>
     )
 }
