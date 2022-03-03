afb6812cab92328580af57ac7984e14a79f5a253
JOSEANDRES14183 <jreyes14183@alumnes.iesmanacor.cat>
2022-03-03 13:58:27 +0100

    recuperar eventos con fecha_ini del calendario en proceso

--- backend_public/app/Http/Controllers/EventoController.php
+++ backend_public/app/Http/Controllers/EventoController.php
@@ -6,15 +6,23 @@ use App\Http\Resources\EventoCollection;
 use App\Http\Resources\EventoResource;
 use App\Models\Evento;
 use Illuminate\Http\Request;
+use phpDocumentor\Reflection\Types\False_;
 
 //Tutorial para controlar que datos aparecen en el JSON
 //https://laravel.com/docs/9.x/eloquent-resources
 
 class EventoController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        return EventoResource::collection(Evento::where('esta_oculto',false)->get());
+        if (!$request->date){
+            return EventoResource::collection(Evento::where('esta_oculto',false)->get());
+        }
+
+        return EventoResource::collection(Evento::where('esta_oculto',false)
+            ->whereHas('sesiones', function ($sesiones){
+                $sesiones->;
+            })->get());
     }
 
     public function show($id)
