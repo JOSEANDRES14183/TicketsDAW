ff67b6064e0a7889e3347ad4a9a0bd6618f853ee
JOSEANDRES14183 <jreyes14183@alumnes.iesmanacor.cat>
2022-03-05 15:44:09 +0100

    añadida pagina de compra que reciba el id de la operacion y hace axios para recuperarla

---
+++ frontend/src/pages/Compra.js
@@ -0,0 +1,44 @@
+import {useParams} from "react-router-dom";
+import {useEffect, useState} from "react";
+import axios from "axios";
+import {Spinner} from "reactstrap";
+import Loading from "../components/Loading";
+
+function Compra(){
+
+    const [isLoading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+    const [operacionCompra, setOperacionCompra] = useState([]);
+
+    const params = useParams();
+
+    useEffect(()=>{
+        axios.get(process.env.REACT_APP_API_PROTOCOL_PREFIX + process.env.REACT_APP_API_HOST + "/api/purchase/"+params.id)
+            .then(result => {
+                setOperacionCompra(result.data);
+                setLoading(false);
+            })
+            .catch(error => {
+                setError(error);
+            });
+    }, [params]);
+
+    if(error){
+        return <p>{error}</p>
+    }
+
+    if (isLoading){
+        return (
+            <Loading />
+        );
+    }
+
+    return (
+        <section className={"container-md"}>
+            <p>Esta es la pagina de compra</p>
+        </section>
+    );
+}
+
+export default Compra;
