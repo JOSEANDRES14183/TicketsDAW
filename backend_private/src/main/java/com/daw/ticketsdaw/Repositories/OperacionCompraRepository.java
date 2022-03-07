7e2d85e2b75a94a1f825cf32fcc51a94329ee910
JOSEANDRES14183 <jreyes14183@alumnes.iesmanacor.cat>
2022-03-07 22:20:00 +0100

    solo tiene en cuenta operaciones de compra finalizadas

--- backend_private/src/main/java/com/daw/ticketsdaw/Repositories/OperacionCompraRepository.java
+++ backend_private/src/main/java/com/daw/ticketsdaw/Repositories/OperacionCompraRepository.java
@@ -8,6 +8,6 @@ import java.util.List;
 
 public interface OperacionCompraRepository extends JpaRepository<OperacionCompra, Integer> {
 
-    @Query(value = "select * from operacion_compra where month(fecha_compra)=?1",nativeQuery = true)
+    @Query(value = "select * from operacion_compra where month(fecha_compra)=?1 and esta_finalizada=1",nativeQuery = true)
     public List<OperacionCompra> getAllOfMonth(int month);
 }
