31cead3282322f7c72b35d22334f8654b23860ef
JOSEANDRES14183 <jreyes14183@alumnes.iesmanacor.cat>
2022-03-08 20:55:06 +0100

    arreglado color de las letras al pasar a modo oscuro

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -47,17 +47,17 @@ function Header(props){
                             <Nav className={"mx-auto"}
                                  navbar>
                                 <NavItem>
-                                    <NavLink href={"/"}>
+                                    <NavLink href={"/"} className={value==="light" ? "text-primary" : "text-white"}>
                                         {t('events')}
                                     </NavLink>
                                 </NavItem>
                                 <NavItem>
-                                    <NavLink href={"/soporte"}>
+                                    <NavLink href={"/soporte"} className={value==="light" ? "text-primary" : "text-white"}>
                                         {t('support')}
                                     </NavLink>
                                 </NavItem>
                                 <NavItem>
-                                    <NavLink href={"/about-us"}>
+                                    <NavLink href={"/about-us"} className={value==="light" ? "text-primary" : "text-white"}>
                                         {t('about-us')}
                                     </NavLink>
                                 </NavItem>
